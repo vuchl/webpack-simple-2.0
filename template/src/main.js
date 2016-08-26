4b806f02a1a72dc3a364cfa4db618ce67b4d749a
Sebastian <vuchl@gmx.de>
2016-08-26 07:02:27 +0200

    Setup for router and store usage

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,42 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+const store = new Vuex.Store({
+	state: {
+		a: 'happy'
+	}
+})
+
+const Home = Vue.component('home', {
+	template: `
+		<span>home</span>
+	`
+})
+
+const Test = Vue.component('test', {
+	template: `
+		<span>test</span>
+	`
+})
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{path: '/', component: Home},
+		{path: '/test', component: Test}
+	]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  template: `
+  	<router-view></router-view>
+	`,
+	store,
+	router
 })
